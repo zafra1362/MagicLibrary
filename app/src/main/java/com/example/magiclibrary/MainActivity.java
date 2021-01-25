fe3ee3d25d3cf27553cd90768c06d38f51ebe7e7
Dylan <dzafrar01@elpuig.xeill.net>
2021-01-25 13:04:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/magiclibrary/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.magiclibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.magiclibrary.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding;
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.login || destination.getId() == R.id.registro || destination.getId() == R.id.acerca || destination.getId() == R.id.theshattering || destination.getId() == R.id.arthas || destination.getId() == R.id.voljin) {
+                binding.bottomNavView.setVisibility(View.GONE);
+            } else {
+                binding.bottomNavView.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
