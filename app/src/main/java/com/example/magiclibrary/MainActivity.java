9cc7bde097bfd3984b075cf2ab9aa2ec057dad4b
Dylan <dzafrar01@elpuig.xeill.net>
2021-01-26 18:01:15 +0100

    Colores y muestras de los libros

--- app/src/main/java/com/example/magiclibrary/MainActivity.java
+++ app/src/main/java/com/example/magiclibrary/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
         NavigationUI.setupWithNavController(binding.bottomNavView, navController);
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-            if (destination.getId() == R.id.login || destination.getId() == R.id.registro || destination.getId() == R.id.acerca || destination.getId() == R.id.theshattering || destination.getId() == R.id.voljin || destination.getId() == R.id.leerarthas || destination.getId() == R.id.leertheshattering || destination.getId() == R.id.leervoljin) {
+            if (destination.getId() == R.id.login || destination.getId() == R.id.registro || destination.getId() == R.id.acerca || destination.getId() == R.id.theshattering || destination.getId() == R.id.voljin || destination.getId() == R.id.leerarthas || destination.getId() == R.id.leertheshattering || destination.getId() == R.id.leervoljin || destination.getId() == R.id.arthas) {
                 binding.bottomNavView.setVisibility(View.GONE);
             } else {
                 binding.bottomNavView.setVisibility(View.VISIBLE);
